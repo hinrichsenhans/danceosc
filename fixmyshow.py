2a4fabe27a891afca569cbb077f5a1b3e372b803
Hans Hinrichsen <hh@etcconnect.com>
2018-04-28 14:48:23 -0500

    Initial version

---
+++ fixmyshow.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+from OSC import *
+
+import sys
+import time
+
+c = OSCStreamingClient()
+#c.connect(('127.0.0.1', 3032))
+c.connect(('10.101.93.101', 3032))
+
+def eos_out_handler(addr, tags, stuff, source):
+	1	
+c.addMsgHandler('default', eos_out_handler)
+#c.sendOSC(OSCMessage("/eos/fader/1/config/10"))
+
+#c.sendOSC(OSCMessage("/eos/fader/1/2/fire"))
+
+
+#read file to get the list of names
+
+if len(sys.argv) != 2 :
+	print('Invalid number of arguments. Usage: fixmyshow.py filename.csv')
+	exit(1)	
+f = open(sys.argv[1], 'r')
+try:
+
+	with open(sys.argv[1], 'r') as f:
+		for line in f:
+			res = line.split('\t')
+			list_num = res[0].strip()
+			act_name = res[1].strip()
+			act_desc = res[2].strip()
+			print("{} {} ({})".format(list_num, act_name, act_desc)) 
+			c.sendOSC(OSCMessage("/eos/newcmd", "Cue 99 /  Copy_To Cue {} / Enter".format(list_num)))
+			c.sendOSC(OSCMessage("/eos/set/cuelist/{}/label".format(list_num), "{} ({})".format(act_name, act_desc)))	
+		time.sleep(1)
+		c.close()	
+		sys.exit()
+except:
+	c.close()
+	sys.exit()
+#get unique names
+
+#write csv with name, index, uuid
+
+
+# r"/eos/newcmd=Cue 99 /  Copy_To Cue 3 / Enter"
+# r"/eos/set/cuelist/3/label=BEN"
+#quit()
---
+++ stichmyshow.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+from OSC import *
+
+import sys
+import time
+
+c = OSCStreamingClient()
+#c.connect(('127.0.0.1', 3032))
+c.connect(('10.101.93.101', 3032))
+
+def eos_out_handler(addr, tags, stuff, source):
+	1	
+c.addMsgHandler('default', eos_out_handler)
+#c.sendOSC(OSCMessage("/eos/fader/1/config/10"))
+
+#c.sendOSC(OSCMessage("/eos/fader/1/2/fire"))
+
+
+#read file to get the list of names
+
+if len(sys.argv) != 3 :
+	print('Invalid number of arguments. Usage: fixmyshow.py filename.csv')
+	exit(1)	
+songs = dict()
+try:
+	with open(sys.argv[1], 'r') as f:
+		for line in f:
+			res = line.split('\t')
+			list_num = res[0].strip()
+			act_name = res[1].strip()
+			act_desc = res[2].strip()
+			#print("{} {} ({})".format(list_num, act_name, act_desc)) 
+			songs[act_name] = list_num
+			# c.sendOSC(OSCMessage("/eos/set/cuelist/{}/label".format(list_num), "{} ({})".format(act_name, act_desc)))	
+		time.sleep(1)
+		#c.close()	
+		#sys.exit()
+except:
+	c.close()
+	sys.exit()
+
+try :
+	with open(sys.argv[2], 'r') as f:
+		previous = -1
+		for line in f:
+			if previous == -1 :
+				print("START")
+			res = line.split('\t')
+			incoming_act = res[1].strip()
+			#print(incoming_act)
+			if not incoming_act.upper() in songs :
+				print("SKIPPING {}".format(incoming_act))
+				continue
+			incoming_num = songs[incoming_act.upper()]
+			print("{}: Act {} list {} previous {}".format(res[0], incoming_act, incoming_num, previous)) 
+			if not previous == -1:
+				c.sendOSC(OSCMessage("/eos/newcmd", "Cue {} / 100  Link Cue {} / Enter".format(previous, incoming_num)))
+			previous = songs[incoming_act.upper()]
+	time.sleep(1)
+	c.close()
+except Exception as e: 
+	print("EXCEPTION")
+	print(e)
+	time.sleep(1)
+	c.close()
+	sys.exit()
+
+sys.exit()
